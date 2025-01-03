5d56844b7a5883ea4785a43c33a254f90c981f0d
Lucy Ouma <akinyilucy09@gmail.com>
2025-01-03 06:46:15 +0300

    Caching updates

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -10,7 +10,7 @@ from base_caching import BaseCaching
 
 class FIFOCache(BaseCaching):
     '''A class `FIFOCache` that inherits from
-    `BaseCaching` and is a caching system.
+       `BaseCaching` and is a caching system.
     '''
 
     def __init__(self):
@@ -19,7 +19,7 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         '''assign to the dictionary `self.cache_data` the
-            `item` value for the key `key`
+           `item` value for the key `key`
         '''
 
         if key is None or item is None:
